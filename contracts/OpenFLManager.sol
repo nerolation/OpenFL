9a839c17d5990f89da937747dc92836a6439390e
Toni Wahrstätter <51536394+nerolation@users.noreply.github.com>
2023-04-11 10:57:12 +0200

    Update OpenFLManager

--- contracts/OpenFLManager.sol
+++ contracts/OpenFLManager.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 //
-//  _______           __       ________  _____     
-// |_   __ \         [  |     |_   __  ||_   _|    
-//   | |__) |__   _   | |.--.   | |_ \_|  | |      
-//   |  ___/[  | | |  | '/'`\ \ |  _|     | |   _  
-//  _| |_    | \_/ |, |  \__/ |_| |_     _| |__/ | 
-// |_____|   '.__.'_/[__;.__.'|_____|   |________| 
-// 
-// PubFL is a Ethereum-based reputation system to facilitate federated learning. 
-// This contract is part of the PubFL research paper by Anton Wahrstätter. The contracts do only 
+//   ___                   _____ _     
+//  / _ \ _ __   ___ _ __ |  ___| |    
+// | | | | '_ \ / _ \ '_ \| |_  | |    
+// | |_| | |_) |  __/ | | |  _| | |___ 
+//  \___/| .__/ \___|_| |_|_|   |_____|
+//       |_| 
+// OpenFL is a Ethereum-based reputation system to facilitate federated learning. 
+// This contract is part of the OpenFL research paper by Anton Wahrstätter. The contracts do only 
 // represent Proof-of-Concepts and have not been developed to be used in productive
 // environments. Do not use them, except for testing purpose.                                              
 
 pragma solidity =0.8.9;
 
-import "./PubFLModel.sol";
+import "./OpenFLModel.sol";
 
-contract PubFLManager {
+contract OpenFLManager {
     
     mapping(address => mapping(uint256 => address)) public ModelOf;
     mapping(address => uint256) public ModelCountOf;
@@ -27,7 +26,7 @@ contract PubFLManager {
                          uint8 _min_rounds, uint8 _punishfactor, uint8 _freeriderPenalty) public payable {
         ModelCountOf[msg.sender] += 1;
         require(msg.value >= _reward + _min_collateral, "NEV");
-        PubFLModel model = new PubFLModel{value:_reward}(_modelHash,
+        OpenFLModel model = new OpenFLModel{value:_reward}(_modelHash,
                                                          _min_collateral, 
                                                          _max_collateral, 
                                                          _reward,
